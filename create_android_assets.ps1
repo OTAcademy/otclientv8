41c7f459440c83ae8667a06f5cb8d09dba155263
bbarwik <bartosz.barwikowski@logpass.me>
2021-04-09 00:44:51 +0200

    Added mods to create_android_assets.ps1 [skip release]

--- create_android_assets.ps1
+++ create_android_assets.ps1
@@ -1,5 +1,5 @@
 $compress = @{
-LiteralPath= "init.lua", "data", "modules", "layouts"
+LiteralPath= "init.lua", "data", "modules", "layouts", "mods"
 CompressionLevel = "Fastest"
 DestinationPath = "android\otclientv8\assets\data.zip"
 }
