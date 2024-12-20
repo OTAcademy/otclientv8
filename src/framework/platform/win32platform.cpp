fe23a364df4dec3e3af35a00526eac91820e7bc8
Arkadiusz Lach <lach@includespark.eu>
2024-12-20 22:00:23 +0100

    Fixed getOSName not working with new Windows versions

--- src/framework/platform/win32platform.cpp
+++ src/framework/platform/win32platform.cpp
@@ -202,248 +202,104 @@ double Platform::getMemoryUsage()
     return pmc.WorkingSetSize;
 }
 
-#ifndef PRODUCT_PROFESSIONAL
-#define PRODUCT_PROFESSIONAL    0x00000030
-#define VER_SUITE_WH_SERVER     0x00008000
-#define VER_PLATFORM_WIN32s             0
-#define VER_PLATFORM_WIN32_WINDOWS      1
-#define VER_PLATFORM_WIN32_NT           2
-#define PRODUCT_UNDEFINED                           0x00000000
-#define PRODUCT_ULTIMATE                            0x00000001
-#define PRODUCT_HOME_BASIC                          0x00000002
-#define PRODUCT_HOME_PREMIUM                        0x00000003
-#define PRODUCT_ENTERPRISE                          0x00000004
-#define PRODUCT_HOME_BASIC_N                        0x00000005
-#define PRODUCT_BUSINESS                            0x00000006
-#define PRODUCT_STANDARD_SERVER                     0x00000007
-#define PRODUCT_DATACENTER_SERVER                   0x00000008
-#define PRODUCT_SMALLBUSINESS_SERVER                0x00000009
-#define PRODUCT_ENTERPRISE_SERVER                   0x0000000A
-#define PRODUCT_STARTER                             0x0000000B
-#define PRODUCT_DATACENTER_SERVER_CORE              0x0000000C
-#define PRODUCT_STANDARD_SERVER_CORE                0x0000000D
-#define PRODUCT_ENTERPRISE_SERVER_CORE              0x0000000E
-#define PRODUCT_ENTERPRISE_SERVER_IA64              0x0000000F
-#define PRODUCT_BUSINESS_N                          0x00000010
-#define PRODUCT_WEB_SERVER                          0x00000011
-#define PRODUCT_CLUSTER_SERVER                      0x00000012
-#define PRODUCT_HOME_SERVER                         0x00000013
-#define PRODUCT_STORAGE_EXPRESS_SERVER              0x00000014
-#define PRODUCT_STORAGE_STANDARD_SERVER             0x00000015
-#define PRODUCT_STORAGE_WORKGROUP_SERVER            0x00000016
-#define PRODUCT_STORAGE_ENTERPRISE_SERVER           0x00000017
-#define PRODUCT_SERVER_FOR_SMALLBUSINESS            0x00000018
-#define PRODUCT_SMALLBUSINESS_SERVER_PREMIUM        0x00000019
-#define PRODUCT_HOME_PREMIUM_N                      0x0000001A
-#define PRODUCT_ENTERPRISE_N                        0x0000001B
-#define PRODUCT_ULTIMATE_N                          0x0000001C
-#define PRODUCT_WEB_SERVER_CORE                     0x0000001D
-#define PRODUCT_MEDIUMBUSINESS_SERVER_MANAGEMENT    0x0000001E
-#define PRODUCT_MEDIUMBUSINESS_SERVER_SECURITY      0x0000001F
-#define PRODUCT_MEDIUMBUSINESS_SERVER_MESSAGING     0x00000020
-#define PRODUCT_SERVER_FOUNDATION                   0x00000021
-#define PRODUCT_HOME_PREMIUM_SERVER                 0x00000022
-#define PRODUCT_SERVER_FOR_SMALLBUSINESS_V          0x00000023
-#define PRODUCT_STANDARD_SERVER_V                   0x00000024
-#define PRODUCT_DATACENTER_SERVER_V                 0x00000025
-#define PRODUCT_ENTERPRISE_SERVER_V                 0x00000026
-#define PRODUCT_DATACENTER_SERVER_CORE_V            0x00000027
-#define PRODUCT_STANDARD_SERVER_CORE_V              0x00000028
-#define PRODUCT_ENTERPRISE_SERVER_CORE_V            0x00000029
-#define PRODUCT_HYPERV                              0x0000002A
-#define PRODUCT_STORAGE_EXPRESS_SERVER_CORE         0x0000002B
-#define PRODUCT_STORAGE_STANDARD_SERVER_CORE        0x0000002C
-#define PRODUCT_STORAGE_WORKGROUP_SERVER_CORE       0x0000002D
-#define PRODUCT_STORAGE_ENTERPRISE_SERVER_CORE      0x0000002E
-#define PRODUCT_STARTER_N                           0x0000002F
-#define PRODUCT_PROFESSIONAL                        0x00000030
-#define PRODUCT_PROFESSIONAL_N                      0x00000031
-#define PRODUCT_SB_SOLUTION_SERVER                  0x00000032
-#endif
-
 std::string Platform::getOSName()
 {
-    typedef void (WINAPI* PGNSI)(LPSYSTEM_INFO);
-    typedef BOOL(WINAPI* PGPI)(DWORD, DWORD, DWORD, DWORD, PDWORD);
+    typedef LONG(WINAPI* RtlGetVersionPtr)(PRTL_OSVERSIONINFOW);
 
-    std::string ret;
-    OSVERSIONINFOEX osvi;
-    SYSTEM_INFO si;
-    PGNSI pGNSI;
-    PGPI pGPI;
-    BOOL bOsVersionInfoEx;
-    DWORD dwType;
+    RTL_OSVERSIONINFOEXW osInfo = { 0 };
+    osInfo.dwOSVersionInfoSize = sizeof(osInfo);
 
-    ZeroMemory(&si, sizeof(SYSTEM_INFO));
-    ZeroMemory(&osvi, sizeof(OSVERSIONINFOEX));
-
-    osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
-    bOsVersionInfoEx = VerifyVersionInfo(&osvi, 0, 0);
-
-    if (!bOsVersionInfoEx)
+    HMODULE hNtDll = GetModuleHandleA("ntdll.dll");
+    if (hNtDll == NULL)
         return std::string();
 
-    pGNSI = (PGNSI)GetProcAddress(GetModuleHandle(TEXT("kernel32.dll")), "GetNativeSystemInfo");
-    if (NULL != pGNSI)
-        pGNSI(&si);
-    else
-        GetSystemInfo(&si);
+    RtlGetVersionPtr RtlGetVersion = (RtlGetVersionPtr)GetProcAddress(hNtDll, "RtlGetVersion");
+    if (RtlGetVersion == NULL)
+        return std::string();
 
-    if (VER_PLATFORM_WIN32_NT == osvi.dwPlatformId && osvi.dwMajorVersion > 4) {
-        if (osvi.dwMajorVersion == 6) {
-            if (osvi.dwMinorVersion == 0) {
-                if (osvi.wProductType == VER_NT_WORKSTATION)
-                    ret += "Windows Vista ";
-                else ret += "Windows Server 2008 ";
-            }
+    if (RtlGetVersion((PRTL_OSVERSIONINFOW)&osInfo) != 0)
+        return std::string();
 
-            if (osvi.dwMinorVersion == 1 || osvi.dwMinorVersion == 2) {
-                if (osvi.wProductType == VER_NT_WORKSTATION && osvi.dwMinorVersion == 1)
-                    ret += "Windows 7 ";
-                else
-                    if (osvi.wProductType == VER_NT_WORKSTATION && osvi.dwMinorVersion == 2)
-                        ret += "Windows 8 ";
-                    else
-                        ret += "Windows Server 2008 R2 ";
-            }
+    DWORD productType = 0;
+    typedef BOOL(WINAPI* GetProductInfoPtr)(DWORD, DWORD, DWORD, DWORD, PDWORD);
+    HMODULE hKernel32 = GetModuleHandleA("kernel32.dll");
+    if (hKernel32 == NULL)
+        return std::string();
 
-            pGPI = (PGPI)GetProcAddress(GetModuleHandle(TEXT("kernel32.dll")), "GetProductInfo");
+    GetProductInfoPtr GetProductInfo = (GetProductInfoPtr)GetProcAddress(hKernel32, "GetProductInfo");
 
-            pGPI(osvi.dwMajorVersion, osvi.dwMinorVersion, 0, 0, &dwType);
+    if (GetProductInfo)
+        GetProductInfo(osInfo.dwMajorVersion, osInfo.dwMinorVersion, 0, 0, &productType);
 
-            switch (dwType) {
-            case PRODUCT_ULTIMATE:
-                ret += "Ultimate Edition";
-                break;
-            case PRODUCT_PROFESSIONAL:
-                ret += "Professional";
-                break;
-            case PRODUCT_HOME_PREMIUM:
-                ret += "Home Premium Edition";
-                break;
-            case PRODUCT_HOME_BASIC:
-                ret += "Home Basic Edition";
-                break;
-            case PRODUCT_ENTERPRISE:
-                ret += "Enterprise Edition";
-                break;
-            case PRODUCT_BUSINESS:
-                ret += "Business Edition";
-                break;
-            case PRODUCT_STARTER:
-                ret += "Starter Edition";
-                break;
-            case PRODUCT_CLUSTER_SERVER:
-                ret += "Cluster Server Edition";
-                break;
-            case PRODUCT_DATACENTER_SERVER:
-                ret += "Datacenter Edition";
-                break;
-            case PRODUCT_DATACENTER_SERVER_CORE:
-                ret += "Datacenter Edition (core installation)";
-                break;
-            case PRODUCT_ENTERPRISE_SERVER:
-                ret += "Enterprise Edition";
-                break;
-            case PRODUCT_ENTERPRISE_SERVER_CORE:
-                ret += "Enterprise Edition (core installation)";
-                break;
-            case PRODUCT_ENTERPRISE_SERVER_IA64:
-                ret += "Enterprise Edition for Itanium-based Systems";
-                break;
-            case PRODUCT_SMALLBUSINESS_SERVER:
-                ret += "Small Business Server";
-                break;
-            case PRODUCT_SMALLBUSINESS_SERVER_PREMIUM:
-                ret += "Small Business Server Premium Edition";
-                break;
-            case PRODUCT_STANDARD_SERVER:
-                ret += "Standard Edition";
-                break;
-            case PRODUCT_STANDARD_SERVER_CORE:
-                ret += "Standard Edition (core installation)";
-                break;
-            case PRODUCT_WEB_SERVER:
-                ret += "Web Server Edition";
-                break;
-            }
-        }
-
-        if (osvi.dwMajorVersion == 5 && osvi.dwMinorVersion == 2) {
-            if (GetSystemMetrics(SM_SERVERR2))
-                ret += "Windows Server 2003 R2, ";
-            else if (osvi.wSuiteMask & VER_SUITE_STORAGE_SERVER)
-                ret += "Windows Storage Server 2003";
-            else if (osvi.wSuiteMask & VER_SUITE_WH_SERVER)
-                ret += "Windows Home Server";
-            else if (osvi.wProductType == VER_NT_WORKSTATION && si.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_AMD64)
-                ret += "Windows XP Professional x64 Edition";
-            else
-                ret += "Windows Server 2003, ";
-
-            if (osvi.wProductType != VER_NT_WORKSTATION) {
-                if (si.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_IA64) {
-                    if (osvi.wSuiteMask & VER_SUITE_DATACENTER)
-                        ret += "Datacenter Edition for Itanium-based Systems";
-                    else if (osvi.wSuiteMask & VER_SUITE_ENTERPRISE)
-                        ret += "Enterprise Edition for Itanium-based Systems";
-                }
-
-                else if (si.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_AMD64) {
-                    if (osvi.wSuiteMask & VER_SUITE_DATACENTER)
-                        ret += "Datacenter x64 Edition";
-                    else if (osvi.wSuiteMask & VER_SUITE_ENTERPRISE)
-                        ret += "Enterprise x64 Edition";
-                    else
-                        ret += "Standard x64 Edition";
-                } else {
-                    if (osvi.wSuiteMask & VER_SUITE_COMPUTE_SERVER)
-                        ret += "Compute Cluster Edition";
-                    else if (osvi.wSuiteMask & VER_SUITE_DATACENTER)
-                        ret += "Datacenter Edition";
-                    else if (osvi.wSuiteMask & VER_SUITE_ENTERPRISE)
-                        ret += "Enterprise Edition";
-                    else if (osvi.wSuiteMask & VER_SUITE_BLADE)
-                        ret += "Web Edition";
-                    else ret += "Standard Edition";
-                }
-            }
-        }
-
-        if (osvi.dwMajorVersion == 5 && osvi.dwMinorVersion == 1) {
-            ret += "Windows XP ";
-            if (osvi.wSuiteMask & VER_SUITE_PERSONAL)
-                ret += "Home Edition";
-            else ret += "Professional";
-        }
-
-        if (osvi.dwMajorVersion == 5 && osvi.dwMinorVersion == 0) {
-            ret += "Windows 2000 ";
-            if (osvi.wProductType == VER_NT_WORKSTATION) {
-                ret += "Professional";
-            } else {
-                if (osvi.wSuiteMask & VER_SUITE_DATACENTER)
-                    ret += "Datacenter Server";
-                else if (osvi.wSuiteMask & VER_SUITE_ENTERPRISE)
-                    ret += "Advanced Server";
-                else ret += "Server";
-            }
-        }
-
-        ret += stdext::format(" (build %d)", osvi.dwBuildNumber);
-
-        if (osvi.dwMajorVersion >= 6) {
-            if (si.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_AMD64)
-                ret += ", 64-bit";
-            else if (si.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_INTEL)
-                ret += ", 32-bit";
-        }
-    } else {
-        ret = "Windows";
+    std::string osName = "Windows ";
+    if (osInfo.dwMajorVersion == 10) {
+        if (osInfo.dwBuildNumber >= 22000)
+            osName += "11";
+        else
+            osName += "10";
     }
-    return ret;
-}
+    else if (osInfo.dwMajorVersion == 6) {
+        if (osInfo.dwMinorVersion == 3)
+            osName += "8.1";
+        else if (osInfo.dwMinorVersion == 2)
+            osName += "8";
+        else if (osInfo.dwMinorVersion == 1)
+            osName += "7";
+        else if (osInfo.dwMinorVersion == 0) {
+            if (osInfo.wProductType == VER_NT_WORKSTATION)
+                osName += "Vista";
+            else {
+                osName += "Server 2008";
+                switch (productType) {
+                case PRODUCT_STANDARD_SERVER: osName += " Standard"; break;
+                case PRODUCT_ENTERPRISE_SERVER: osName += " Enterprise"; break;
+                case PRODUCT_DATACENTER_SERVER: osName += " Datacenter"; break;
+                case PRODUCT_WEB_SERVER: osName += " Web Edition"; break;
+                default: osName += " Edition";
+                }
+            }
+        }
+    }
+    else if (osInfo.dwMajorVersion == 5) {
+        if (osInfo.dwMinorVersion == 2)
+            osName += "Server 2003/XP x64";
+        else if (osInfo.dwMinorVersion == 1)
+            osName += "XP";
+        else
+            osName += "2000";
+    }
+    else {
+        osName += "Unknown";
+    }
 
+    SYSTEM_INFO si;
+    GetNativeSystemInfo(&si);
+
+    switch (si.wProcessorArchitecture) {
+    case PROCESSOR_ARCHITECTURE_AMD64: {
+        osName += ", 64-bit";
+        break;
+    }
+    case PROCESSOR_ARCHITECTURE_INTEL: {
+        osName += ", 32-bit";
+        break;
+    }
+    case PROCESSOR_ARCHITECTURE_ARM64: {
+        osName += ", ARM64";
+        break;
+    }
+    case PROCESSOR_ARCHITECTURE_ARM: {
+        osName += ", ARM";
+        break;
+    }
+    default: {
+        osName += ", Unknown Architecture";
+        break;
+    }
+    }
+
+    return osName;
+}
 
 std::string Platform::traceback(const std::string& where, int level, int maxDepth)
 {
