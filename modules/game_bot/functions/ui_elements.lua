9775fc4fc735df6f77110d57c4ed53ea262a6b6f
divinity76 <hans@loltek.net>
2026-01-20 01:16:30 +0100

    vBot item list scroll to bottom on new entry (#81)
    previously when adding a new item to a large list,
    the list would scroll to the top each time.
    For very long lists with a lot of stuff to add,
     this becomes increasingly annoying and time-consuming for each new entry,
     to scroll back to the bottom to add a new entry.
    The amount of scrolling requires increase for every 4 new entries.

--- modules/game_bot/functions/ui_elements.lua
+++ modules/game_bot/functions/ui_elements.lua
@@ -26,6 +26,13 @@ UI.Container = function(callback, unique, parent, widget)
   
   local oldItems = {}
   
+  local scrollToBottom = function()
+    local scrollbar = widget.scroll
+    if scrollbar and scrollbar.setValue and scrollbar.getMaximum then
+      scrollbar:setValue(scrollbar:getMaximum())
+    end
+  end
+
   local updateItems = function()
     local items = widget:getItems()
 
@@ -72,6 +79,7 @@ UI.Container = function(callback, unique, parent, widget)
     for i, child in ipairs(widget.items:getChildren()) do
       child.onItemChange = updateItems
     end
+    scrollToBottom()
   end
   
   widget.getItems = function()
